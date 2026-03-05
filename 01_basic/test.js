f18077e5fda9b92bd5ae5a6d82f6d8153637d6eb
devsaini08 <155433889+devsaini08@users.noreply.github.com>
2026-03-05 18:39:25 +0000

    learn variables

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,21 @@
 console.log("hellow bhai")
+
+let accNumber = "123";
+var firstName = "dev";
+const pasWoord = 23214;
+let wifeName;
+
+
+firstName = "sid";
+accNumber = 34321;
+// pasWoord = 789;  const can't  reasign and redelceare
+// var can reasign and redelceare
+// let cant reasign but can't redeclares
+
+
+
+
+console.table([accNumber, firstName, pasWoord, wifeName]);
+
+
+
