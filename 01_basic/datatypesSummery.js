76a08a83a8f1c7aab240a8fcb21a997aefe0b109
devsaini08 <155433889+devsaini08@users.noreply.github.com>
2026-03-07 06:24:04 +0000

    memoy (heap and stack)

--- 01_basic/datatypesSummery.js
+++ 01_basic/datatypesSummery.js
@@ -28,3 +28,33 @@ console.log(obj);
 // array = object
 // obj = object
 // function = function 
+
+// ************************************************************************************
+
+// stack(primitive data types)    heap(non - primitive data types)   =>
+
+//                 ===> Valur chnaged but in first variable have old value
+//                 ===> because stack memory take copy value 
+let str = "nameABC"
+let str2 = str
+str2 = "nameDEF"
+
+// console.log(str);
+// console.log(str2);
+
+
+
+// ====> in this heap memory oldear variable also changed because this will take refrence 
+let demoObj = {
+    name: "rahul",
+    upiID: "rahul@ybk"
+}
+
+let demoObj2 = demoObj
+demoObj2.name = "sagar"
+
+console.log(demoObj);
+console.log(demoObj2);
+
+
+
