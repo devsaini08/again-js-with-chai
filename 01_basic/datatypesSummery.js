4efb4754c58196a816daa0083bf9e7ca636df874
devsaini08 <155433889+devsaini08@users.noreply.github.com>
2026-03-07 05:59:52 +0000

    datatype summery

---
+++ 01_basic/datatypesSummery.js
@@ -0,0 +1,30 @@
+//primitive data types =>
+
+//  7 => string , number ,boolean , null , undefined, symbel, bigInt
+let num = 25;              // Number
+let name = "Sid";          // String
+let isStudent = true;      // Boolean
+let value;                 // Undefined
+let empty = null;          // Null
+let bigNumber = 123n;      // BigInt
+let unique = Symbol("34"); // Symbol
+
+// console.log(unique);
+
+
+// refrence (non primitive data type)
+
+// 3 ==> Aaray, Object, Function
+
+let arr = [1, 4, 6, 64, 33,]
+
+let obj = {
+    name: "dev",
+    age: 27,
+}
+console.log(obj);
+
+// datatype of primitive data types is = function , and function is called obj function
+// array = object
+// obj = object
+// function = function 
