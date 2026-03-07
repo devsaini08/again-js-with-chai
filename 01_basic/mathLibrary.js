f28006f658d33dc06e3255388da973d4082b1347
devsaini08 <155433889+devsaini08@users.noreply.github.com>
2026-03-07 18:33:53 +0000

    math libaray

---
+++ 01_basic/mathLibrary.js
@@ -0,0 +1,22 @@
+let num = 876.7
+let arrOfNum = [2, 4, 5, 3, 57, 43, 86]
+let a = 8
+let b = 6
+let c = 9
+
+// console.log(Math.abs(-8));       // to change nagitive value to positive
+// console.log(Math.round(num));    // to round of value 
+// console.log(Math.floor(num));    // round of to lower side 
+// console.log(Math.ceil(num));     // round of to upper side
+// console.log(Math.pow(num, 2));   // to calculate the power
+
+
+// console.log(Math.min(...arrOfNum));  // for find max and min value of aaray
+// console.log(Math.max(...arrOfNum));
+
+// console.log(Math.max(a, b, c));     // for find max and min value of muntiple variable
+
+ let gwernate  console.log(Math.floor(Math.random() * 10000) + 1);
+
+
+
---
+++ 01_basic/numberAdv.js
@@ -0,0 +1,20 @@
+let num = 9876465.998
+let newNUm = new Number(456789)
+
+// console.log(num);
+// console.log(newNUm , typeof newNUm);
+
+
+// console.log(num.toString(),);
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(5));  // this method give approx value
+console.log(num.toLocaleString(`en-IN`)); // (`en-IN`) => india method , () => us method
+  
+
+
+
+
+
+
+
+
